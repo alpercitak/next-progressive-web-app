ab1b2dc339becaa909ffc438a41dbbe56d9a08bb
Alper Çıtak <citak.alper@gmail.com>
2022-07-16 13:33:23 +0300

    PWA implementation

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+const withPWA = require('next-pwa');
+const runtimeCaching = require('next-pwa/cache');
 
-module.exports = nextConfig
+module.exports = withPWA({
+  pwa: {
+    dest: 'public',
+    runtimeCaching,
+    // disable: process.env.NODE_ENV === 'development',
+  },
+});
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
+import Head from 'next/head'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
